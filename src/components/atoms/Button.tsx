ee79b2310561442874f3345b9c9c6907cb1f7f1b
behzad888 <behzad88.2012@gmail.com>
2022-07-20 13:24:06 +0430

    add button component

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+import React, { forwardRef } from "react";
+import { __DEV__ } from "utils";
+
+import "assets/elements/button.scss";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean | React.ReactNode;
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  const { loading, className, children, ...restProps } = props;
+
+  return (
+    <button
+      role="button"
+      ref={ref}
+      className={clsx("c-btn", className)}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+});
+
+Button.defaultProps = {
+  type: "button",
+};
+
+if (__DEV__) {
+  Button.displayName = "Button";
+}
+
+export { Button };
